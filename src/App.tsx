0f8cb51c62ca9baf17fde624025233359b646a4f
FayJu <faizan.badruddin.bardai@gmail.com>
2023-08-07 17:05:23 +0200

    long url input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { TextField } from "@mui/material";
+import { Container } from "@mui/material";
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <Container maxWidth="md">
+      <TextField
+        fullWidth
+        id="long-url"
+        label="Long URL"
+        multiline
+        maxRows={4}
+        minRows={4}
+        autoFocus
+        helperText="Enter a long URL to shorten"
+        margin="dense"
+        placeholder="https://www.example.com/very-long-url"
+        variant="filled"
+      />
+    </Container>
+  );
 }
 
 export default App;
