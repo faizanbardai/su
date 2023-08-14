15ee3d928925690afd04fb3adcdbf4f548c07a6b
FayJu <faizan.badruddin.bardai@gmail.com>
2023-08-14 13:27:31 +0200

    redirect after clearing history

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { historyData } from "../types";
 import SingleHistoryCard from "../components/history/SingleHistoryCard";
 import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const History = () => {
+  const navigate = useNavigate();
   const [history, setHistory] = useState(
     JSON.parse(localStorage.getItem("history") || "[]")
   );
@@ -11,12 +13,15 @@ const History = () => {
   const clearHistory = () => {
     localStorage.setItem("history", JSON.stringify([]));
     setHistory([]);
+    navigate("/");
   };
   return (
     <Box>
-      <Box sx={{ textAlign: "right", mb: 1 }}>
-        <Button onClick={clearHistory}>Clear History</Button>
-      </Box>
+      {history.length > 0 && (
+        <Box sx={{ textAlign: "right", mb: 1 }}>
+          <Button onClick={clearHistory}>Clear History</Button>
+        </Box>
+      )}
       {history.map((h: historyData, i: number) => (
         <SingleHistoryCard key={i} data={h} />
       ))}
